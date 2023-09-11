63c2746b06f248e21149d64fbb3c901f2c848dc8
Cosmas Wasswa <cosywas@gmail.com>
2023-09-11 18:01:43 +0300

    Add game.rb with properties and methods and associations with Item class

---
+++ classes/game.rb
@@ -0,0 +1,22 @@
+require_relative 'item'
+require 'Date'
+
+class game < Item
+    attr_accessor :multiplayer, :last_played_at
+
+    def initialize(publish_date, multiplayer, last_played_at)
+        super(publish_date)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+
+    end
+
+    private
+    def can_be_archived?
+        @now = Date.today
+        @date = Date.parse(@last_played_at)
+        @last_played = ((@now-@date)/365).floor
+        return true if move_to_archive && @last_played > 2
+
+    end
+end
