bbcdfdd4739ddfe1e8a3a29b2f2e46c6581b580e
Cosmas Wasswa <cosywas@gmail.com>
2023-09-11 13:49:03 +0300

    Fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -1,28 +1,28 @@
 require 'json/add/date'
 class Item
+  attr_accessor :id, :genre, :label, :author
 
-attr_accessor :id, :genre, :label, :author
-def initialize(publish_date, archived: false)
+  def initialize(publish_date, archived: false)
     @id = Random.rand(1..1000)
     @genre = genre
     @label = label
     @author = author
     @publish_date = publish_date
     @archived = archived
-end
+  end
 
-private
-def can_be_archived?
+  private
+
+  def can_be_archived?
     @today = Date.today
     @date = Date.parse(@publish_date)
     ((@today - @date) / 365).floor > 10
-end
+  end
 
-public
+  public
 
   def move_to_archive
     @archived = true if can_be_archived?
     @archived
   end
-
 end
