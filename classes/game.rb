79b1e741e650a44811a89e47acf21448a5bde9cc
Cosmas Wasswa <cosywas@gmail.com>
2023-09-11 18:09:21 +0300

    Correct syntax error on classes

--- classes/game.rb
+++ classes/game.rb
@@ -1,22 +1,21 @@
 require_relative 'item'
 require 'date'
 
-class game < Item
-    attr_accessor :multiplayer, :last_played_at
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize(publish_date, multiplayer, last_played_at)
-        super(publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    end
+  private
 
-    private
-    def can_be_archived?
-        @now = Date.today
-        @date = Date.parse(@last_played_at)
-        @last_played = ((@now-@date)/365).floor
-        return true if move_to_archive && @last_played > 2
-
-    end
+  def can_be_archived?
+    @now = Date.today
+    @date = Date.parse(@last_played_at)
+    @last_played = ((@now - @date) / 365).floor
+    supper && @last_played > 2
+  end
 end
