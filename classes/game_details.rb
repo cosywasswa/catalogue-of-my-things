8c480239d983b9d2a7211b0185e87d0c3e518785
Cosmas Wasswa <cosywas@gmail.com>
2023-09-12 11:09:54 +0300

    Add gamedetails class with extra properties for game name and authou

---
+++ classes/game_details.rb
@@ -0,0 +1,21 @@
+require_relative 'game'
+
+class GameDetails < Game
+
+    @@games = []
+
+    attr_accessor :game_name, :first_name, :last_name
+   def initialize(game_name, first_name, last_name, publish_date, multiplayer, last_played_at)
+    super( publish_date, multiplayer, last_played_at) 
+    @game_name = game_name
+    @first_name = first_name
+    @last_name = last_name
+
+    @@games << self
+end
+
+def self.list_games
+    @@games
+  end
+
+end
