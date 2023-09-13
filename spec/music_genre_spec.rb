5038aea58b4f256093b68d5e53b590e5d56195ba
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-13 08:53:56 +0000

    Fix linter errors

--- spec/music_genre_spec.rb
+++ spec/music_genre_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 require_relative '../classes/music_genre'
 
 RSpec.describe Genre do
-    let(:genre) {Genre.new('Jalloh')}
-    describe '#initialize' do
-        it 'returns the correct_name of the genre in music Albums' do
-            expect(genre.name).to eq('Jalloh')
-        end
+  let(:genre) { Genre.new('Jalloh') }
+  describe '#initialize' do
+    it 'returns the correct_name of the genre in music Albums' do
+      expect(genre.name).to eq('Jalloh')
     end
+  end
 end
