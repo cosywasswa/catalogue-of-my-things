9e8ab37e9b791bd72d1a96228f42b726a29214e2
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-09-13 14:01:25 +0200

    Update save_data_to_json method to save only book data

--- classes/label.rb
+++ classes/label.rb
@@ -14,4 +14,19 @@ class Label
     @items << item
     item.label = self
   end
+
+  def to_json(*_args)
+    {
+      title: @title,
+      color: @color
+    }.to_json
+  end
+
+  def self.from_json(json_data)
+    data = JSON.parse(json_data)
+    new(
+      data['title'],
+      data['color']
+    )
+  end
 end
